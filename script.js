26fb63db74457ae50824a48c793891f87b1e1b2f
satanrout <satyaranjanrout81@gmail.com>
2020-05-04 06:48:59 +0530

    a responsive nav bar which can be done with 3lines of vanila javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('#ham');
+const nav = document.querySelector("ul");
+
+burger.addEventListener('click', function() {
+  nav.classList.toggle('active');
+});
+
+// //this is the first one but I think beginners can easily understand the upper one.
+// const burger = document.querySelector('#ham');
+// const nav = document.querySelector("ul");
+// burger.addEventListener('click', () => {nav.classList.toggle('active');});
